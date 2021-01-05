c00c1494aabc4f5ed1341c8ca95bdd644761d9e2
Noud de Brouwer <noud4@home.nl>
2021-01-05 23:06:44 +0100

    storeLinkSettings keep options

--- client/src/utils/links.js
+++ client/src/utils/links.js
@@ -1,10 +1,8 @@
 export function linkStringForFrontEnd(linkString, pluralEntityName) {
-  console.log('string', linkString);
   return linkString.replace(encodeURIComponent('/' + pluralEntityName + '/'), '');
 }
 
-export function storeLinkSettings(pluralEntityName) {
-  let options = null;
-  options = {entity: pluralEntityName};
+export function storeLinkSettings(pluralEntityName, options = {}) {
+  options.entity = pluralEntityName;
   return options;
 }
